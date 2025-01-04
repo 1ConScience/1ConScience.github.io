c10540d82ac3eefa6188eb0ef2f3b2d79aac97ea
1ConScience <32107547+1ConScience@users.noreply.github.com>
2025-01-04 22:14:29 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,36 @@
+from lvl1 import *
+from lvl2 import *
+from lvl3 import *
+from lvl4 import *
+from lvl5 import *
+from lvl6 import *
+from lvl7 import *
+from lvl8 import *
+
+
+lvl7()
+nettoyage()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+lvl8()
+nettoyage()
+
+"""
+lvl1()
+nettoyage()
+lvl3()
+nettoyage()
+lvl4()
+nettoyage()
+lvl5()
+nettoyage()
+lvl2()
+nettoyage()
+lvl6()
+nettoyage()
+lvl7()
+nettoyage()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+lvl8()
+nettoyage()
+
+"""
